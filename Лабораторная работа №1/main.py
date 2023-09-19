5ba4b513577c0ab3c0bc02214fa095657ad6b1f4
ITprog3r <92672347+Mixey2005@users.noreply.github.com>
2023-09-19 16:19:40 +0300

    Add files via upload

--- Лабораторная работа №1/main.py
+++ Лабораторная работа №1/main.py
@@ -1,13 +1,29 @@
-
-
+print('51 простое число')
 def t1():
-    print('51 простое число')
+    def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
 
-def reverse_string(text):
-        reversed_string = text[::-1]
+    def find_51st_prime():
+        count = 0
+        number = 2
+        while True:
+            if is_prime(number):
+                count += 1
+                if count == 51:
+                    return number
+            number += 1
+    fifty_first_prime = find_51st_prime()
+    print("51ое простое число:",fifty_first_prime)
 
-        print("Реверсированная строка: ",reversed_string)
 def t2():
+    def reverse_string(text):
+        reversed_string = text[::-1]
+        return reversed_string
     text = str(input("Введите текст:"))
     char_count = {}
     for char in text:
@@ -19,7 +35,7 @@ def t2():
         most_common_char = max(char_count, key=char_count.get)
 
     print("Наиболее повторяющийся символ в строке: ",most_common_char)
-    reverse_string(text)
+    print("Реверсия строки:",reverse_string(text))
 def t3():
     my_list = [13, 56, 'Python', 34, 19, 'love']
     for i in range(len(my_list)):
@@ -35,7 +51,7 @@ def t3():
 
     print("Измененный список:", my_list)
 
-#Найти три ключа с самыми маленькими значениями в словаре
+
 def t4():
     my_dict = {'a': 50, 'b': 5, 'c': 56, 'd': 4, 'e': 58, 'f': 20}
     sorted_dict = sorted(my_dict.items(), key=lambda x: x[1])
@@ -47,7 +63,7 @@ def t4():
 def t5():
     toy_store = {
         'Машинка': ['Бип-бип', 100, 10],
-        'Кукла': ['Раздень меня полностью', 200, 5],
+        'Кукла': ['Поиграй со мной', 200, 5],
         'Мишка': ['Где мой мёд?', 150, 8],
         'Лего': ['Собери меня', 800, 12],
         'Мяч': ['Поиграем в футбол', 250, 3],
@@ -56,12 +72,12 @@ def t5():
 
     while True:
         print("\nМеню:")
-        print("1. Просмотр описания")
-        print("2. Просмотр цены")
-        print("3. Просмотр количества")
-        print("4. Вся информация")
-        print("5. Покупка")
-        print("6. Выход")
+        print("1.Просмотр описания")
+        print("2.Просмотр цены")
+        print("3.Просмотр количества")
+        print("4.Вся информация")
+        print("5.Покупка")
+        print("6.Выход")
 
         choice = input("Выберите пункт меню: ")
 
@@ -92,19 +108,23 @@ def t5():
                 print(f"{toy_name}: {toy_info[0]}, Цена: {toy_info[1]}, Количество: {toy_info[2]}")
 
         elif choice == '5':
-            toy_name = input("Введите название игрушки для покупки (или 'n' для выхода): ")
-            if toy_name == 'n':
-                break
-            if toy_name in toy_store:
-                quantity = int(input("Введите количество: "))
-                if quantity <= toy_store[toy_name][2]:
-                    cost = toy_store[toy_name][1] * quantity
-                    toy_store[toy_name][2] -= quantity
-                    print(f"Покупка успешно совершена. Сумма к оплате: {cost}")
+            total_cost = 0
+            print("Покупка игрушек:")
+            while True:
+                toy_name = input("Введите название игрушки для покупки (или 'n' для выхода): ")
+                if toy_name == 'n':
+                    break
+                if toy_name in toy_store:
+                    quantity = int(input("Введите количество: "))
+                    if quantity <= toy_store[toy_name][2]:
+                        cost = toy_store[toy_name][1] * quantity
+                        toy_store[toy_name][2] -= quantity
+                        total_cost += cost
+                        print(f"Покупка успешно совершена. Сумма к оплате: {total_cost}")
+                    else:
+                        print("Извините, товара в таком количестве нет в наличии.")
                 else:
-                    print("Извините, товара в таком количестве нет в наличии.")
-            else:
-                print("Игрушка не найдена.")
+                    print("Игрушка не найдена.")
 
         elif choice == '6':
             print("До свидания!")
@@ -113,23 +133,21 @@ def t5():
         else:
             print("Неверный выбор. Пожалуйста, выберите пункт меню от 1 до 6.")
 def t6():
-    # Заданный кортеж целых чисел
     my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-    # Инициализируем переменную для хранения суммы четных элементов
     even_sum = 0
 
-    # Проходим по элементам кортежа
     for num in my_tuple:
         if num % 2 == 0:
             even_sum += num
 
-    # Выводим сумму четных элементов
     print("Сумма четных элементов:", even_sum)
 
-ch = input("Введите номер задания: ")
+ch = input("Введите номер задания(0 - чтобы выйти): ")
 while True:
-    if ch == '1':
+    if ch == '0':
+        break
+    elif ch == '1':
         t1()
     elif ch == '2':
         t2()
